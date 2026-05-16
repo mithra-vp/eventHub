235d29500b1aee2ee70ae0a15512ae2b3d4a8847
mithra-vp <mithravp004@gmail.com>
2026-05-16 06:06:02 +0530

    first commit

--- backend/routes/reviewRoute.js
+++ backend/routes/reviewRoute.js
@@ -4,15 +4,12 @@ const { protect } = require("../middlewares/authMiddleware");
 
 const reviewRoute = Router();
 
-// Public
 reviewRoute.get("/latest", reviewController.getLatestReviews);
 reviewRoute.get("/event/:eventId", reviewController.getEventReviews);
 reviewRoute.get("/event/:eventId/stats", reviewController.getEventRatingStats);
 
-// Protected: current user's reviews
 reviewRoute.get("/mine", protect, reviewController.myReviews);
 
-// User only (protected) - controller enforces role=user and paid booking
 reviewRoute.post("/", protect, reviewController.createReview);
 
 module.exports = reviewRoute;
